867e4cf2b2006c77d4cf398820b927c2da9a53b5
Chris Hulton <chrishulton@gmail.com>
2015-04-28 23:26:07 -0400

    yahoo callbacks?

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :users, controllers: { sessions: "users/sessions" }
+  devise_for :users, controllers: { sessions: "users/sessions", :omniauth_callbacks => "users/omniauth_callbacks" }
   get 'homes/index'
 
   # The priority is based upon order of creation: first created -> highest priority.
